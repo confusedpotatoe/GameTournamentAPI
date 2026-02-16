f9ecf3aa13396d900c55ebfa03e9bb5e96b537f2
confusedpotatoe <milo.arvaa@gmail.com>
2026-02-16 17:40:41 +0100

    added interface

---
+++ Services/TournamentService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GameTournamentAPI.Data;
+using GameTournamentAPI.Models;
+
+namespace GameTournamentAPI.Services
+{
+    public class TournamentService : ITournamentService
+    {
+        private readonly AppDbContext _context;
+
+    }
+}
