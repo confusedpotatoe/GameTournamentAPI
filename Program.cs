1d1f0e34a76a7d753c2492cea47cb85aceed8507
confusedpotatoe <milo.arvaa@gmail.com>
2026-02-16 18:13:39 +0100

    added swagger

--- Program.cs
+++ Program.cs
@@ -12,23 +12,31 @@ namespace GameTournamentAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddScoped<ITournamentService, TournamentService>();
+
             builder.Services.AddDbContext<Data.AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
-            
-            builder.Services.AddScoped<ITournamentService, TournamentService>();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
