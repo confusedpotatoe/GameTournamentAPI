2a407521bfe7ac8ce63bbb2e0d9fa81e21e68904
confusedpotatoe <milo.arvaa@gmail.com>
2026-02-16 18:01:00 +0100

    controllers created.
    first migration done

---
+++ Controllers/TournamentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameTournamentAPI.Services;
+using GameTournamentAPI.Models;
+
+namespace GameTournamentAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class TournamentController : Controller
+    {
+        private readonly ITournamentService _tournamentService;
+
+        public TournamentController(ITournamentService tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] string? search)
+        {
+            var tournaments = await _tournamentService.GetAllAsync(search);
+            return Ok(tournaments.Select(t => new { t.Id }));
+        }
+
+    }
+}
