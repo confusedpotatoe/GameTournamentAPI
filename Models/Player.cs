4497b76866669d88f9f0918f32701e987cf634a3
confusedpotatoe <milo.arvaa@gmail.com>
2026-02-20 13:44:02 +0100

    added player modell

---
+++ Models/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace GameTournamentAPI.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        //foreign key to tournament
+        public int TournamentId { get; set; }
+
+        //navigation property
+        public Tournament? Tournament { get; set; }
+    }
+}
